ce8c6b2e645b0e2c6356c9d919874441819c803c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-31 11:07:02 +0200

    Building a list were we can delete from

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,39 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
     var body: some View {
-        Text("Swift")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("Expense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
